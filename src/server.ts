a3ed8f4efb42cb90105b15cd87897ccf38a94d11
--global <--global>
2021-08-09 15:44:39 -0300

    [update] configuracao e dependencias adicionadas pro typescript funcionar direito

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-const express = require('express');
+import express from 'express';
+
 const app = express();
 
 app.listen(3000, ()=>{
     console.log('maquina de sexo eu transo igual um animal');
 })
+
